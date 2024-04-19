c08848887ff2c794c3157792aa28e11fddd46869
teofurlan <105223297+teofurlan@users.noreply.github.com>
2024-04-19 20:59:28 -0300

    Deleted client app given that it was not used in this branch

--- client/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.use(express.static('src'))
-
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
-});
