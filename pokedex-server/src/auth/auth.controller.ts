4cbc62b7df8947bd5d718b5306d5145397c870bb
teofurlan <105223297+teofurlan@users.noreply.github.com>
2024-04-28 01:16:13 -0300

    Implemented persistence and ran more tests

--- pokedex-server/src/auth/auth.controller.ts
+++ pokedex-server/src/auth/auth.controller.ts
@@ -14,7 +14,7 @@ export class AuthController {
   // Specifies the http response's status to be 200 because by POST's default one for successful request is 201 (CREATED), that's why no @HttpCode() decorator was used in the signup endpoint. However, when a user signs in, nothing is created so it's not accurate.
   @HttpCode(HttpStatus.OK)
   @Post('signin')
-  signin(@Body user: AuthDto) {
+  signin(@Body() user: AuthDto) {
     return this.authService.signin(user);
   }
 }
--- pokedex-server/src/auth/auth.service.ts
+++ pokedex-server/src/auth/auth.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import Datastore from 'nedb-promises';
+import { AuthDto } from './dto';
 
-console.log(Datastore);
 const usersDB = Datastore.create({
   filename: 'src/database/users.db',
   autoload: true,
@@ -11,13 +11,28 @@ const usersDB = Datastore.create({
 export class AuthService {
   constructor() {}
 
-  async signup() {
+  async signup(user: AuthDto) {
     try {
-      return
-    } catch (error) {}
+      const existingUser = await usersDB.findOne({ email: user.email });
+      if (!existingUser) {
+        usersDB.insert(user);
+        return user;
+      }
+      throw new ForbiddenException('This email is already in use');
+    } catch (error) {
+      throw error;
+    }
   }
 
-  async signin() {
-    return 'You signed in successfully!';
+  async signin(user: AuthDto) {
+    try {
+      const existingUser = await usersDB.findOne({ email: user.email });
+      if (existingUser) {
+        return user;
+      }
+      throw new ForbiddenException('Credentials incorrect');
+    } catch (error) {
+      throw error;
+    }
   }
 }
