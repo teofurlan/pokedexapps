d1c3c8964a30035f4fbd328ca975a9a23afce76e
teofurlan <105223297+teofurlan@users.noreply.github.com>
2024-04-28 00:02:52 -0300

    Deleted AuthModule's import in the AppModule

--- pokedex-server/src/app.module.ts
+++ pokedex-server/src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PokemonModule } from './pokemon/pokemon.module';
-import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PokemonModule, AuthModule, UsersModule],
+  imports: [PokemonModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
