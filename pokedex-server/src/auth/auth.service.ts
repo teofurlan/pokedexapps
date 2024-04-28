447ef79373684b49427c0890c29ead5f8589960a
teofurlan <105223297+teofurlan@users.noreply.github.com>
2024-04-28 09:45:54 -0300

    Hashed the password and restructured

--- pokedex-server/src/auth/auth.service.ts
+++ pokedex-server/src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { ForbiddenException, Injectable } from '@nestjs/common';
 import Datastore from 'nedb-promises';
 import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
 const usersDB = Datastore.create({
   filename: 'src/database/users.db',
@@ -11,24 +12,34 @@ const usersDB = Datastore.create({
 export class AuthService {
   constructor() {}
 
-  async signup(user: AuthDto) {
+  async signup(dto: AuthDto) {
     try {
-      const existingUser = await usersDB.findOne({ email: user.email });
+      // We check if the user exists in the database first
+      const existingUser = await usersDB.findOne({ email: dto.email });
+      // If it does not exist, we insert it into the db
       if (!existingUser) {
-        usersDB.insert(user);
+        // Generates a hash for the user's password
+        const hash = await argon.hash(dto.password);
+        // Saves the user's email in the database and its respective hash
+        const user = usersDB.insert({ email: dto.email, hash });
         return user;
       }
+      // If it does exits, then we throw and exception
       throw new ForbiddenException('This email is already in use');
     } catch (error) {
       throw error;
     }
   }
 
-  async signin(user: AuthDto) {
+  async signin(dto: AuthDto) {
     try {
-      const existingUser = await usersDB.findOne({ email: user.email });
+      // First, we check if the email is valid. Passes { _id: 0 } as second argument to avoid the query to return the db's id
+      const existingUser = await usersDB.findOne(
+        { email: dto.email },
+        { _id: 0 },
+      );
       if (existingUser) {
-        return user;
+        return existingUser;
       }
       throw new ForbiddenException('Credentials incorrect');
     } catch (error) {
