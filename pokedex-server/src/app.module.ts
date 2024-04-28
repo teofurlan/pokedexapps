d38931d62778bbf7bc363334feeaa7604b2b2610
teofurlan <105223297+teofurlan@users.noreply.github.com>
2024-04-28 00:18:08 -0300

    Created auth resources

--- pokedex-server/src/app.module.ts
+++ pokedex-server/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PokemonModule, UsersModule],
+  imports: [PokemonModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
