6bc77114e2487e15906df2578f78845481a9fe42
teofurlan <105223297+teofurlan@users.noreply.github.com>
2024-04-28 00:44:19 -0300

    Set basic structure for testing the endpoints

--- pokedex-server/src/auth/auth.controller.ts
+++ pokedex-server/src/auth/auth.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('signup')
+  signup() {
+    return this.authService.signup();
+  }
+
+  // Specifies the http response's status to be 200 because by POST's default one for successful request is 201 (CREATED), that's why no @HttpCode() decorator was used in the signup endpoint. However, when a user signs in, nothing is created so it's not accurate.
+  @HttpCode(HttpStatus.OK)
+  @Post('signin')
+  signin() {
+    return this.authService.signin();
+  }
+}
--- pokedex-server/src/auth/auth.service.ts
+++ pokedex-server/src/auth/auth.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import Datastore from 'nedb-promises';
+
+console.log(Datastore);
+const usersDB = Datastore.create({
+  filename: 'src/pokemon/database/database.db',
+  autoload: true,
+});
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor() {}
+
+  async signup() {
+    return 'Your account was created successfully!';
+  }
+
+  async signin() {
+    return 'You signed in successfully!';
+  }
+}
