e009d177c40f54bcbc58f30ebbb26a20843a524b
teofurlan <105223297+teofurlan@users.noreply.github.com>
2024-04-28 23:02:50 -0300

    Deleted react routes

--- reactapp/src/App.tsx
+++ reactapp/src/App.tsx
@@ -1,15 +1,9 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Pokedex from "./Pokedex";
-import { Signup } from "./Signup";
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Pokedex />}></Route>
-        <Route path="/signup" element={<Signup />}></Route>
-      </Routes>
-    </Router>
+    <>
+      <Pokedex />
+    </>
   );
 }
