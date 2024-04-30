4f6ab8f547ae3b58e5f1bb6e25c1db34b8495a59
teofurlan <105223297+teofurlan@users.noreply.github.com>
2024-04-30 19:32:52 -0300

    Deleted old server

--- server/src/helpers/errors.ts
+++
@@ -1,4 +0,0 @@
-export const invalidInput = 'Invalid input';
-export const nameTooLong = 'Name is too long';
-export const nameTooShort = 'Name is too short';
-export const pokemonAlreadyExists = 'Pokemon already exists'
--- server/src/helpers/hashPassword.ts
+++
@@ -1,9 +0,0 @@
-import crypto from 'crypto';
-
-export function getSalt() {
-  return crypto.randomBytes(16).toString('hex');
-}
-
-export function hashPassword(password: string) {
-  return crypto.createHash('sha256').update(password).digest('hex');
-}
